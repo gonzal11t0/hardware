34ae880abb434471fad216c665a11750beedcce1
Gonzalo Lazarte <gonzalaz@live.com.ar>
2024-02-21 18:15:20 -0300

    settings

--- src/App.js
+++ src/App.js
@@ -57,7 +57,6 @@ function App() {
         <Router>
           <Menu />
           <Routes>
-            <Route path="/" element={<Navigate to="/Home" />} />
             <Route path="/Home" element={<Home />} />
             <Route path="/Notebook" element={<Notebook />} />
             <Route path="/Pcs" element={<Pcs />} />
